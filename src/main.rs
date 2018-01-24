56ee4f77f62407c965421d13016d41b6aac959ee
syzygy1 <3028851+syzygy1@users.noreply.github.com>
2018-01-25 00:21:58 +0100

    Work around stack overflow problem. This should fix https://github.com/syzygy1/Rustfish/issues/1

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ mod types;
 mod uci;
 mod ucioption;
 
+use std::thread;
+
 fn main() {
     println!("{}", misc::engine_info(false));
 
@@ -39,7 +41,12 @@ fn main() {
     tb::init(ucioption::get_string("SyzygyPath"));
     search::clear();
 
-    uci::cmd_loop();
+    // To avoid a stack overflow, we create a thread with a large
+    // enough stack size to run the UI.
+    let builder = thread::Builder::new().stack_size(16 * 1024 * 1024);
+    let ui_thread = builder.spawn(|| uci::cmd_loop()).unwrap();
+    let _ = ui_thread.join();
+    // uci::cmd_loop();
 
     threads::free();
     tb::free();
